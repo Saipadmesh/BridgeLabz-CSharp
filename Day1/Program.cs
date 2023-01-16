f42ca920630983e469866775b88f0b94cc2ce138
V Saipadmesh <60597851+Saipadmesh@users.noreply.github.com>
2023-01-16 22:03:32 +0530

    Added the code files for day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // WageCalculator.GenerateReport();
+            var L1 = new Line(1, 2, 0, 5);
+            var L2 = new Line(5, 3, -1, 6);
+            LineComparison.Compare(L1, L2);
+            
+        }
+    }
+}
---
+++ Day1/WageCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public static class WageCalculator
+{
+	public static void GenerateReport()
+	{
+        int WAGEPERHOUR = 20;
+        int FULLDAYHOURS = 8, HALFDAYHOURS = 4;
+        int WORKINGDAYSPERMONTH = 20, CurrentWorkingDaysPerMonth = 0;
+        int WORKINGHOURS = 100, CurrentWorkingHours = 0;
+        int TotalWage = 0;
+
+        Console.Write("Attendance Report: ");
+        while (CurrentWorkingHours < WORKINGHOURS && CurrentWorkingDaysPerMonth < WORKINGDAYSPERMONTH)
+        {
+            // generating attendance
+            Random random = new Random();
+            int attendance = random.Next(0, 2);
+            int hours = random.Next(0, 2);
+            Console.Write(attendance + ",");
+
+            switch (attendance)
+            {
+                case 1: CurrentWorkingHours += (hours == 1) ? FULLDAYHOURS : HALFDAYHOURS; break;
+                default: continue;
+            }
+
+            // calculating wage
+            TotalWage = CurrentWorkingHours * WAGEPERHOUR;
+            CurrentWorkingDaysPerMonth++;
+        }
+        Console.WriteLine("\n");
+        Console.WriteLine("Total Wage: " + TotalWage);
+    }
+}

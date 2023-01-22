1ad72b979882907ac49a005fea2e55850b34c0b5
V Saipadmesh <60597851+Saipadmesh@users.noreply.github.com>
2023-01-22 15:30:05 +0530

    Addressbook with add, update, and delete functions

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,71 @@
+﻿
+
+namespace FirstProject
+{
+    public class AddressBook
+    {
+        List<Contact> contacts;
+        public AddressBook() {
+            contacts= new List<Contact>();
+        }
+
+        public List<Contact> GetList()
+        {
+            return contacts;
+        }
+
+        public void Add(Contact contact)
+        {
+            string firstName=contact.FirstName;
+            if(!contacts.Where(contact1=>contact1.FirstName==firstName).Any())
+            {
+                contacts.Add(contact);
+            }
+            else
+            {
+                Console.WriteLine("Contact with same name exists");
+            }
+        }
+        public void Remove(string firstName)
+        {
+            // UC4
+            var toDeleteList = contacts.Where((contact) => contact.FirstName == firstName);
+            if(toDeleteList.Any() )
+            {
+                contacts.Remove(toDeleteList.First());
+            }
+            else
+            {
+                Console.WriteLine("No contact with the given first name is found");
+            }
+        }
+        public void Edit(string firstName, string phNo="", string address="", string city="", string state="")
+        {
+            var toEditList = contacts.Where((contact)=>contact.FirstName == firstName);
+            if(toEditList.Any() )
+            {
+                Contact toEdit = toEditList.First();
+                if (phNo.Length > 0)
+                {
+                    toEdit.Phone = phNo;
+                }
+                if (address.Length > 0)
+                {
+                    toEdit.Address = address;
+                }
+                if (city.Length > 0)
+                {
+                    toEdit.City = city;
+                }
+                if (state.Length > 0)
+                {
+                    toEdit.State = state;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Contact does not exist");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FirstProject
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+
+            addressBook.Add(new Contact("Sai", "Padmesh", "Sivaram Nagar", "Coimbatore", "Tamil Nadu", "641045", "8072669384", "saipadmesh@gmail.com"));
+            addressBook.Add(new Contact("Ram", "Mohan", "Sivaram Nagar", "Coimbatore", "Tamil Nadu", "641045", "9042143893", "ramm@gmail.com"));
+            //addressBook.Add(new Contact("Ram", "Mohan", "Sivaram Nagar", "Coimbatore", "Tamil Nadu", "641045", "9042143893", "ramm@gmail.com"));
+            addressBook.Edit("Ram", phNo: "123456789");
+            foreach (var contact in addressBook.GetList())
+            {
+                Console.WriteLine(contact.Phone);
+            }
+            addressBook.Remove("Ram");
+        }
+
+    }
+}
